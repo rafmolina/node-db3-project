1bd15e9fc08a983a482e1cd2e7b6991c5a73aed5
Rafael Molina <seissaysseis@users.noreply.github.com>
2021-02-17 19:21:14 -0700

    working on finding steps

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,44 @@
 // scheme-model
+
+const db = require('../../data/db-config')
+
+function find() {
+    return db('schemes')
+}
+
+function findById(id) {
+    return db('schemes').where("id", id).first()
+}
+
+//
+
+function add(scheme) {
+    return db('schemes').insert(scheme)
+    .then(([id]) => {
+        return db('schemes').where("id", id).first()
+    })
+}
+
+function update(id, scheme) {
+    const postId =id
+    return db('schemes').where("id", id).update(scheme)
+    .then(() => {
+        return db('schemes').where("id", postId).fist()
+    })
+}
+
+function remove(id) {
+    return db('schemes').where("id", id).del()
+    .then(() => {
+        return db('schemes')
+    })
+}
+
+module.exports= {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
